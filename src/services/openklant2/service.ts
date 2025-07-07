8f0a12309a8a832755773d18acb2f7036e918f30
Henok Solomon <henok@icatt.nl>
2025-07-07 19:40:59 +0300

    Enforce maximum length of 180 characters for specifieke vraag in saveKlantContact function

--- src/services/openklant2/service.ts
+++ src/services/openklant2/service.ts
@@ -40,6 +40,7 @@ const klantinteractiesKlantcontacten = `${klantinteractiesBaseUrl}/klantcontacte
 const klantinteractiesActoren = `${klantinteractiesBaseUrl}/actoren`;
 const klantinteractiesDigitaleadressen = `${klantinteractiesBaseUrl}/digitaleadressen`;
 const klantinteractiesBetrokkenen = `${klantinteractiesBaseUrl}/betrokkenen`;
+const MAX_SPECIFIEKE_VRAAG = 180;
 
 ////////////////////////////////////////////
 // contactmomenten and contactverzoeken
@@ -486,6 +487,9 @@ export const saveKlantContact = async (
   systemIdentifier: string,
   vraag: Vraag,
 ): Promise<SaveKlantContactResponseModel> => {
+  if (vraag.specifiekevraag.length > MAX_SPECIFIEKE_VRAAG) {
+    throw new Error("Specifieke vraag mag maximaal 180 tekens bevatten.");
+  }
   const klantcontactPostModel: KlantContactPostmodel = {
     kanaal: vraag.kanaal,
     onderwerp:
