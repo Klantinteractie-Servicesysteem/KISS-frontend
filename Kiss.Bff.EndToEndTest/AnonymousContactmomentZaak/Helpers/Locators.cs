8a945446ce9d3d6340d436525f41f1a1ec2d2d4f
surbhijha92 <surbhi@info.nl>
2025-08-06 10:17:14 +0200

    fixed scripts

--- Kiss.Bff.EndToEndTest/AnonymousContactmomentZaak/Helpers/Locators.cs
+++ Kiss.Bff.EndToEndTest/AnonymousContactmomentZaak/Helpers/Locators.cs
@@ -4,7 +4,7 @@
     public static class Locators
     {
         // This locator does not use an ARIA role. Consider updating the locator to use an ARIA role for better accessibility.
-        public static ILocator FirstContactMomentSummary(this IPage page) => page.Locator("table.overview tbody tr");
+        public static ILocator FirstContactMomentSummary(this IPage page) => page.Locator("ul.overview > li:nth-child(2) summary");
         // This locator does not use an ARIA role. Consider updating the locator to use an ARIA role for better accessibility.
         public static ILocator GespreksresultaatHeader(this ILocator parentLocator) => parentLocator.Locator("span[aria-describedby='gespreksresultaat-header']");
         // This locator does not use an ARIA role. Consider updating the locator to use an ARIA role for better accessibility.
