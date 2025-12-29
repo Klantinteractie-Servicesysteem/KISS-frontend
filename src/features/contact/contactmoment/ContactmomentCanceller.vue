739dbba4ef54ec112cae8e92592eaeba8844efaa
Daan Gast <daangast@gmail.com>
2025-12-29 14:37:23 +0100

    Added conditional warning message to ContactMomentCanceller

--- src/features/contact/contactmoment/ContactmomentCanceller.vue
+++ src/features/contact/contactmoment/ContactmomentCanceller.vue
@@ -2,6 +2,7 @@
   <prompt-modal
     :dialog="cancelDialog"
     message="Weet je zeker dat je het contactmoment wilt annuleren? Alle gegevens worden verwijderd."
+    :warning="warningMessage"
     cancel-message="Nee"
     confirm-message="Ja"
   />
@@ -22,6 +23,7 @@ import { Button as UtrechtButton } from "@utrecht/component-library-vue";
 import { useConfirmDialog } from "@vueuse/core";
 import PromptModal from "@/components/PromptModal.vue";
 import { useContactmomentStore } from "@/stores/contactmoment";
+import { computed } from "vue";
 const router = useRouter();
 const cancelDialog = useConfirmDialog();
 const contactmomentStore = useContactmomentStore();
@@ -30,6 +32,15 @@ const navigateToPersonen = () =>
   contactmomentStore.contactmomentLoopt
     ? router.push(contactmomentStore.huidigContactmoment?.route ?? "personen")
     : router.push({ path: "/" });
+
+const warningMessage = computed(() => {
+  const vragen = contactmomentStore.contactmomentLoopt
+    ? contactmomentStore.huidigContactmoment?.vragen
+    : undefined;
+  return vragen && vragen.length > 1
+    ? "Let op: Dit contactmoment bevat meerdere vragen. Als je het contactmoment annuleert worden alle vragen automatisch afgebroken. Het is mogelijk individuele vragen te verwijderen in het afrondscherm."
+    : "";
+});
 
 cancelDialog.onConfirm(() => {
   contactmomentStore.stop();
