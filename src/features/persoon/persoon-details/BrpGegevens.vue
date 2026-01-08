237fad79592ceda067575d549cd5a8767b19b2fe
Daan Gast <daangast@gmail.com>
2026-01-08 17:14:28 +0100

    Changed colors and box layout

--- src/features/persoon/persoon-details/BrpGegevens.vue
+++ src/features/persoon/persoon-details/BrpGegevens.vue
@@ -6,11 +6,11 @@
       messageType="error"
       :message="'Er is een fout opgetreden'"
     />
-    <application-message
-      v-if="persoon"
-      messageType="warning"
-      :message="'Onderstaande gegevens mogen alleen worden gebruikt ter controle van de identiteit van de inwoner. Verstrek nooit de hier getoonde gegevens.'"
-    />
+    <utrecht-paragraph class="attention" v-if="persoon">
+      Onderstaande gegevens mogen alleen worden gebruikt ter controle van de
+      identiteit van de inwoner.<br />
+      Verstrek nooit de hier getoonde gegevens.
+    </utrecht-paragraph>
     <dl v-if="persoon">
       <dt>Naam</dt>
       <dd>
@@ -43,7 +43,10 @@
 
 <script setup lang="ts">
 import { searchPersonen, type Persoon } from "@/services/brp";
-import { Heading as UtrechtHeading } from "@utrecht/component-library-vue";
+import {
+  Heading as UtrechtHeading,
+  Paragraph as UtrechtParagraph,
+} from "@utrecht/component-library-vue";
 import DutchDate from "@/components/DutchDate.vue";
 import { enforceOneOrZero, useLoader } from "@/services";
 import { watchEffect } from "vue";
@@ -77,3 +80,13 @@ watchEffect(() => persoon.value && emit("load", persoon.value));
 watchEffect(() => emit("loading", loading.value));
 watchEffect(() => emit("error", error.value));
 </script>
+
+<style lang="scss" scoped>
+.attention {
+  padding: var(--spacing-default);
+  border: 1px solid var(--color-attention);
+  outline-style: solid;
+  color: var(--color-attention);
+  background-color: var(--color-attention-background);
+}
+</style>
